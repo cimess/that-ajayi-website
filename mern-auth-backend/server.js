7adf509403f277de5c39be0bca1183ca7dd6885c
ThankGod Aimuan Osagie <cimessthemanofvalor@gmail.com>
2025-12-11 13:38:01 +0100

    integrated the backend logic

--- mern-auth-backend/server.js
+++ mern-auth-backend/server.js
@@ -30,12 +30,7 @@ app.use(morgan('dev'));
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:3000",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
-    "http://127.0.0.1:3000"
+   "https://that-ajayi.netlify.app"
   ],
   credentials: true
 }));
